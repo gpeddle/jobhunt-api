fc630bd939a41541deffed6d06c787ee643bd0d4
Greg Peddle <gpeddle@gmail.com>
2022-04-11 07:05:45 -0400

    fix id generation

--- src/JobHunt.Api/Models/JobApplication.cs
+++ src/JobHunt.Api/Models/JobApplication.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JobHunt.Api.Models;
 
 public class JobApplication
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string? Id { get; set; }
     public string? Name { get; set; }
     public List<JobApplicationAnswer>? Answers { get; set; }
--- src/JobHunt.Api/Models/JobApplicationAnswer.cs
+++ src/JobHunt.Api/Models/JobApplicationAnswer.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JobHunt.Api.Models;
 
 public class JobApplicationAnswer
 {
-    public string? Id { get; set; } //Question Id
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string? Id { get; set; } 
+    public string? QuestionId { get; set; }
     public string? Answer { get; set; }
 }
